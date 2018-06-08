c06b57c2a1aab5f92e55ed91c2cfde25ebb6ee40
Johan Degraeve <johan.degraeve@gmail.com>
2018-06-08 22:21:26 +0200

    clean up files that should not be in github and update .gitignore

--- Native Extension/iOS/NativeExtensioniOS/build/NativeExtensioniOS.build/Debug-iphoneos/NativeExtensioniOS.build/Objects-normal/armv7/FQApiObject-FC949A778CA24085.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/FQApiObject.m \
-  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/FQApiObject.h
