51a562d3ef714e46069558883d0218fa253a7066
Miguel Kennedy <miguel.kennedy@caloriejunkies.com>
2018-08-24 01:25:39 +0100

    .

---
+++ Native Extension/iOS/NativeExtensioniOS/build/NativeExtensioniOS.build/Debug-iphoneos/NativeExtensioniOS.build/Objects-normal/arm64/G5Api.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/G5Api.m \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/G5Api.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/G5HeaderDefine.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/FPANEUtils.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/FlashRuntimeExtensions.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/G5BleManager.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/G5ApiObject.h \
+  /Volumes/Data\ HD/Development/GitHub/Spike/Native\ Extension/iOS/NativeExtensioniOS/NativeExtensioniOS/FQToolsUtil.h
