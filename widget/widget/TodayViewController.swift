6333f3b13ac36330ce02daf3d605569fccde22cd
Miguel Kennedy <miguel.kennedy@caloriejunkies.com>
2019-04-11 03:56:18 +0100

    Widget gets App Groups address directly from the mobile provisioning file.

--- widget/widget/TodayViewController.swift
+++ widget/widget/TodayViewController.swift
@@ -141,6 +141,14 @@ class TodayViewController: UIViewController, NCWidgetProviding, PNChartDelegate
          predictionsOutcome = "101"
          */
         
+        //Get App Groups address directly from the mobile provisioning file.
+        if let provision = MobileProvision.read() {
+            let entitlementAppGroup = provision.entitlements.appGroups[0]
+            if (entitlementAppGroup != "")
+            {
+                fileUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: entitlementAppGroup)!
+            }
+        }
         
         //Widget Properties
         chartView.backgroundColor = UIColor.clear
@@ -786,5 +794,126 @@ extension Date
     func toTimestamp() -> Int64!
     {
         return Int64(self.timeIntervalSince1970 * 1000)
+    }
+}
+
+struct MobileProvision: Decodable {
+    var entitlements: Entitlements
+    
+    private enum CodingKeys : String, CodingKey {
+        case entitlements = "Entitlements"
+    }
+    
+    // Sublevel: decode entitlements informations
+    struct Entitlements: Decodable {
+        let appGroups: [String]
+        
+        private enum CodingKeys: String, CodingKey {
+            case appGroups = "com.apple.security.application-groups"
+        }
+        
+        init(appGroups: Array<String>) {
+            self.appGroups = appGroups
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let appGroups: [String] = (try? container.decode([String].self, forKey: .appGroups)) ?? []
+            
+            self.init(appGroups: appGroups)
+        }
+    }
+}
+
+/*struct MobileProvision: Decodable {
+    var name: String
+    var appIDName: String
+    var applicationIdentifierPrefix: [String]
+    var teamIdentifier: [String]
+    var platform: [String]
+    var isXcodeManaged: Bool? = false
+    var creationDate: Date
+    var expirationDate: Date
+    var entitlements: Entitlements
+    
+    private enum CodingKeys : String, CodingKey {
+        case name = "Name"
+        case appIDName = "AppIDName"
+        case applicationIdentifierPrefix = "ApplicationIdentifierPrefix"
+        case teamIdentifier = "TeamIdentifier"
+        case platform = "Platform"
+        case isXcodeManaged = "IsXcodeManaged"
+        case creationDate = "CreationDate"
+        case expirationDate = "ExpirationDate"
+        case entitlements = "Entitlements"
+    }
+    
+    // Sublevel: decode entitlements informations
+    struct Entitlements: Decodable {
+        let keychainAccessGroups: [String]
+        let appGroups: [String]
+        let getTaskAllow: Bool
+        let apsEnvironment: Environment
+        
+        private enum CodingKeys: String, CodingKey {
+            case keychainAccessGroups = "keychain-access-groups"
+            case appGroups = "com.apple.security.application-groups"
+            case getTaskAllow = "get-task-allow"
+            case apsEnvironment = "aps-environment"
+        }
+        
+        enum Environment: String, Decodable {
+            case development, production, disabled
+        }
+        
+        init(keychainAccessGroups: Array<String>, appGroups: Array<String>,getTaskAllow: Bool, apsEnvironment: Environment) {
+            self.keychainAccessGroups = keychainAccessGroups
+            self.appGroups = appGroups
+            self.getTaskAllow = getTaskAllow
+            self.apsEnvironment = apsEnvironment
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let keychainAccessGroups: [String] = (try? container.decode([String].self, forKey: .keychainAccessGroups)) ?? []
+            let appGroups: [String] = (try? container.decode([String].self, forKey: .appGroups)) ?? []
+            let getTaskAllow: Bool = (try? container.decode(Bool.self, forKey: .getTaskAllow)) ?? false
+            let apsEnvironment: Environment = (try? container.decode(Environment.self, forKey: .apsEnvironment)) ?? .disabled
+            
+            self.init(keychainAccessGroups: keychainAccessGroups, appGroups: appGroups, getTaskAllow: getTaskAllow, apsEnvironment: apsEnvironment)
+        }
+    }
+}*/
+
+extension MobileProvision {
+    // Read mobileprovision file embedded in app.
+    static func read() -> MobileProvision? {
+        let profilePath: String? = Bundle.main.path(forResource: "embedded", ofType: "mobileprovision")
+        guard let path = profilePath else { return nil }
+        return read(from: path)
+    }
+    
+    // Read a .mobileprovision file on disk
+    static func read(from profilePath: String) -> MobileProvision? {
+        guard let plistDataString = try? NSString.init(contentsOfFile: profilePath,
+                                                       encoding: String.Encoding.isoLatin1.rawValue) else { return nil }
+        
+        // Skip binary part at the start of the mobile provisionning profile
+        let scanner = Scanner(string: plistDataString as String)
+        guard scanner.scanUpTo("<plist", into: nil) != false else { return nil }
+        
+        // ... and extract plist until end of plist payload (skip the end binary part.
+        var extractedPlist: NSString?
+        guard scanner.scanUpTo("</plist>", into: &extractedPlist) != false else { return nil }
+        
+        guard let plist = extractedPlist?.appending("</plist>").data(using: .isoLatin1) else { return nil }
+        let decoder = PropertyListDecoder()
+        do {
+            let provision = try decoder.decode(MobileProvision.self, from: plist)
+            return provision
+        } catch {
+            // TODO: log / handle error
+            return nil
+        }
     }
 }
