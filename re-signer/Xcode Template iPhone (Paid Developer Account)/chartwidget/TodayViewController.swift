a1da8b391ce4c2d81dd294ea9d0343aab6fae830
Miguel Kennedy <miguel.kennedy@caloriejunkies.com>
2019-04-30 13:45:35 +0100

    Added re-signer Xcode template projects for iPad

---
+++ re-signer/Xcode Template iPhone (Paid Developer Account)/chartwidget/TodayViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodayViewController.swift
+//  chartwidget
+//
+//  Created by Spike on 22/04/2019.
+//  Copyright © 2019 Spike. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
